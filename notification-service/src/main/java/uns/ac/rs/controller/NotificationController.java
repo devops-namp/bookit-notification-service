ed68ed064d2f768a4e1211040d8fb81b72dbb858
NevenaRadesic <102974378+NevenaRadesic@users.noreply.github.com>
2024-06-19 01:31:03 +0200

    refactor: notifications connected to accommodations

--- notification-service/src/main/java/uns/ac/rs/controller/NotificationController.java
+++ notification-service/src/main/java/uns/ac/rs/controller/NotificationController.java
@@ -1,5 +1,6 @@
 package uns.ac.rs.controller;
 
+import io.vertx.core.json.JsonObject;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
@@ -7,6 +8,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import uns.ac.rs.entity.Notification;
+import uns.ac.rs.entity.NotificationEvent;
 import uns.ac.rs.service.NotificationService;
 
 import java.util.List;
@@ -20,11 +22,13 @@ public class NotificationController {
     NotificationService notificationService;
 
     @Incoming("notification-queue")
-    public Response sendNotification(Notification notification) {
-        System.out.println("STIGLA NOTIFIKACIJA");
-        notificationService.sendNotification(notification);
+    public Response getNotification(JsonObject json) {
+        NotificationEvent event = json.mapTo(NotificationEvent.class);
+        System.out.println(event.getText());
+        notificationService.sendNotification(new Notification(event));
         return Response.ok().build();
     }
+
 
     @POST
     @Path("markAsRead/{id}")
